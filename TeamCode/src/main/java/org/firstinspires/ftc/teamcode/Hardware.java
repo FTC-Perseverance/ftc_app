a7955773464469b2f5a779f4c373ed0a5115ba9e
Enes Holz <enes.holz@gmail.com>
2021-11-20 12:50:57 +0100

    First code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.os.AsyncTask;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Hardware {
+    //Create Motors
+    public DcMotor frontRightMotor = null;
+    public DcMotor frontLeftMotor = null;
+    public DcMotor backRightMotor = null;
+    public DcMotor backLeftMotor = null;
+
+    //Additional Variables
+    HardwareMap hardwareMap = null;
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public Hardware(HardwareMap hwMap) {
+        initialize(hwMap);
+
+    }
+
+    private void initialize(HardwareMap hwMap) {
+        hardwareMap = hwMap;
+
+        //Connect Motor
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+
+
+        //Set Up Motor Direction
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Set Motor Mode
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        //set zero power for Motors
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //set motors to use no power
+        frontRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+
+
+    }
+
+    //tying to write something xD
+    public class test extends LinearOpMode {
+
+        @Override
+        public void runOpMode() {
+            telemetry.addData("Status", "Initialized");
+            telemetry.update();
+            //Wait for the game to start
+            waitForStart();
+            //run until the end of the match
+            while (opModeIsActive()) {
+                telemetry.addData("Status", "Running");
+                telemetry.update();
+
+                frontRightMotor.setPower(1);
+                frontLeftMotor.setPower(1);
+                backRightMotor.setPower(1);
+                backLeftMotor.setPower(1);
+
+                sleep(5000);
+
+
+                frontRightMotor.setPower(0);
+                frontLeftMotor.setPower(0);
+                backRightMotor.setPower(0);
+                backLeftMotor.setPower(0);
+
+            }
+        }
+    }
+}
