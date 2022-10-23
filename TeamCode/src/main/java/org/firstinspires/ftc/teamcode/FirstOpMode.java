4d2e401eaffdb611f209003bda0852ea6b34cca8
ENES-PC\enesh <enes.holz@online.de>
2022-10-23 18:33:11 +0200

    First op mode Viper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode;
+import android.os.AsyncTask;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 
 @TeleOp
 public class FirstOpMode extends LinearOpMode {
@@ -16,8 +23,8 @@ public class FirstOpMode extends LinearOpMode {
     private DcMotor backRightMotor = null;
     private DcMotor backLeftMotor = null;
     private DcMotor LinearLiftingMotor = null;
-    private DcMotor ExtentionMotorUp = null;
-    private DcMotor ExtentionMotorDown = null;
+
+    private DistanceSensor DS1 = null;
 
 
     @Override
@@ -31,30 +38,27 @@ public class FirstOpMode extends LinearOpMode {
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
         LinearLiftingMotor = hardwareMap.get(DcMotor.class, "LinearLiftingMotor");
-        ExtentionMotorDown = hardwareMap.get(DcMotor.class, "ExtentionMotorDown");
-        ExtentionMotorUp = hardwareMap.get(DcMotor.class, "ExtentionMotorUp");
 
+        DS1 = hardwareMap.get(DistanceSensor.class,"DS1");
 
         frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         LinearLiftingMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        ExtentionMotorUp.setDirection(DcMotorSimple.Direction.FORWARD);
-        ExtentionMotorDown.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        int LiftingSpace = 0;
+        long time;
+        long time2;
         double vertical;
         double horizontal;
         double platzhalter = gamepad1.right_trigger + gamepad1.left_trigger;
         vertical = -gamepad1.left_stick_y;
         horizontal = gamepad1.left_stick_x;
 
+        Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)DS1;
 
-        //set motors to use power TeleOp tells it to use
-        frontRightMotor.setPower(-vertical + horizontal);
-        frontLeftMotor.setPower(-vertical - horizontal);
-        backRightMotor.setPower(-vertical - horizontal);
-        backLeftMotor.setPower(-vertical + horizontal);
+
 
 
         //Wait for the game to start
@@ -63,6 +67,7 @@ public class FirstOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
+            telemetry.addData("deviceName",DS1.getDeviceName() );
 
 
             if (platzhalter == 0) {
@@ -113,34 +118,134 @@ public class FirstOpMode extends LinearOpMode {
                 backLeftMotor.setPower(0);
             }
             //lifting Motors
-            if (gamepad1.b ==true) {
+            if (gamepad1.a ==true) {
+                if(LiftingSpace == 0){
                 LinearLiftingMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-                LinearLiftingMotor.setPower(0.5);
-            }
-            if( gamepad1.b == false && gamepad1.a ==false){
-                LinearLiftingMotor.setPower(0.10);
-            }
-            if (gamepad1.a== true) {
+                LinearLiftingMotor.setPower(1);
+                time = System.currentTimeMillis();
+                time2 = System.currentTimeMillis();
+                while(time2 < time+1000){
+                    time2 = System.currentTimeMillis();
+                    if (platzhalter == 0) {
+                        vertical = -gamepad1.left_stick_y;
+                        horizontal = gamepad1.left_stick_x;
+                        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        frontRightMotor.setPower((-vertical + horizontal) / 0.5);
+                        frontLeftMotor.setPower((-vertical - horizontal) / 0.5);
+                        backRightMotor.setPower((-vertical - horizontal) / 0.5);
+                        backLeftMotor.setPower((-vertical + horizontal) / 0.5);
+                    }
 
-                LinearLiftingMotor.setPower(0);
-            }
-            if (gamepad1.x == true) {
-                ExtentionMotorDown.setDirection(DcMotorSimple.Direction.FORWARD);
-                ExtentionMotorUp.setDirection(DcMotorSimple.Direction.FORWARD);
-                ExtentionMotorDown.setPower(2);
-                ExtentionMotorUp.setPower(2);
-            }
-            if (gamepad1.x == false && gamepad1.y ==false) {
-                ExtentionMotorUp.setPower(0);
-                ExtentionMotorDown.setPower(0);
+                    //turn right
+                    if (gamepad1.right_trigger > 0.5) {
+                        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                        backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                        frontRightMotor.setPower(0.75);
+                        frontLeftMotor.setPower(0.75);
+                        backRightMotor.setPower(0.75);
+                        backLeftMotor.setPower(0.75);
+                    } else if (gamepad1.right_trigger < 0.5) {
+                        frontRightMotor.setPower(0);
+                        frontLeftMotor.setPower(0);
+                        backRightMotor.setPower(0);
+                        backLeftMotor.setPower(0);
+                    }
+                    //turn left
+                    if (gamepad1.left_trigger > 0.5) {
+                        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                        backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                        frontRightMotor.setPower(0.75);
+                        frontLeftMotor.setPower(0.75);
+                        backRightMotor.setPower(0.75);
+                        backLeftMotor.setPower(0.75);
+                    }
+                    //prevent two commands happen at the same time
+                    else if (gamepad1.left_trigger < 0.4) {
+                        frontRightMotor.setPower(0);
+                        frontLeftMotor.setPower(0);
+                        backRightMotor.setPower(0);
+                        backLeftMotor.setPower(0);
+                    }
 
+                        LinearLiftingMotor.setPower(0);
+                LiftingSpace = 1;
+                } }
+
+
+                if (LiftingSpace == 1){
+                    LinearLiftingMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                    LinearLiftingMotor.setPower(1);
+                    time = System.currentTimeMillis();
+                    time2 = System.currentTimeMillis();
+                    while(time2 < time+1000) {
+                        time2 = System.currentTimeMillis();
+
+                        if (platzhalter == 0) {
+                            vertical = -gamepad1.left_stick_y;
+                            horizontal = gamepad1.left_stick_x;
+                            frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                            frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            frontRightMotor.setPower((-vertical + horizontal) / 0.5);
+                            frontLeftMotor.setPower((-vertical - horizontal) / 0.5);
+                            backRightMotor.setPower((-vertical - horizontal) / 0.5);
+                            backLeftMotor.setPower((-vertical + horizontal) / 0.5);
+                        }
+
+                        //turn right
+                        if (gamepad1.right_trigger > 0.5) {
+                            frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                            frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                            backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                            frontRightMotor.setPower(0.75);
+                            frontLeftMotor.setPower(0.75);
+                            backRightMotor.setPower(0.75);
+                            backLeftMotor.setPower(0.75);
+                        } else if (gamepad1.right_trigger < 0.5) {
+                            frontRightMotor.setPower(0);
+                            frontLeftMotor.setPower(0);
+                            backRightMotor.setPower(0);
+                            backLeftMotor.setPower(0);
+                        }
+                        //turn left
+                        if (gamepad1.left_trigger > 0.5) {
+                            frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                            backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                            frontRightMotor.setPower(0.75);
+                            frontLeftMotor.setPower(0.75);
+                            backRightMotor.setPower(0.75);
+                            backLeftMotor.setPower(0.75);
+                        }
+                        //prevent two commands happen at the same time
+                        else if (gamepad1.left_trigger < 0.4) {
+                            frontRightMotor.setPower(0);
+                            frontLeftMotor.setPower(0);
+                            backRightMotor.setPower(0);
+                            backLeftMotor.setPower(0);
+
+                        }
+                    }
+                    LinearLiftingMotor.setPower(0);
+                    LiftingSpace = 0;
+                }
             }
-            if (gamepad1.y ==true) {
-                ExtentionMotorDown.setDirection(DcMotorSimple.Direction.REVERSE);
-                ExtentionMotorUp.setDirection(DcMotorSimple.Direction.REVERSE);
-                ExtentionMotorDown.setPower(2);
-                ExtentionMotorUp.setPower(2);
+
+
+            if (DS1.getDistance(DistanceUnit.CM) < 20 && LiftingSpace == 1){
+                sleep(1000);
             }
+
 
 
         }
